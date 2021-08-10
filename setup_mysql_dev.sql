d41d7287f67d4cc727390346607961ae1b411910
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-10 17:19:15 +0000

    adding 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+--prepares a MySQL server for the project
+--A database hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--A new user hbnb_dev (in localhost)
+GRANT ALL ON hbnb_dev_db.* to 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--hbnb_dev should have SELECT privilege on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
