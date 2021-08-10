886353f25691479dda121f3127a64dae05108958
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-10 17:24:53 +0000

    fixing 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,6 @@
 --prepares a MySQL server for the project
 --A database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---A new user hbnb_dev (in localhost)
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---hbnb_dev should have SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
