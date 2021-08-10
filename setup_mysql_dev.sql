0976b26626fa59e13dbae4d15b170c9b35067d0b
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-10 17:20:18 +0000

    fixing 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 --A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --A new user hbnb_dev (in localhost)
-GRANT ALL ON hbnb_dev_db.* to 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --hbnb_dev should have SELECT privilege on the database performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
