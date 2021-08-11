dedfe06fd0655d4a0e6cc02c5176f56908d1c974
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-11 19:30:21 +0000

    test 3

--- models/city.py
+++ models/city.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-from models.state import State
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
