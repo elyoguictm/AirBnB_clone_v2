704bf982e7c0c49d0f406f9a1b99dc0622633347
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-11 19:38:04 +0000

    fixing state

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
@@ -12,13 +12,17 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
+        cities = relationship('City', backref="state",
+                              cascade="all, delete-orphan")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
+    if (getenv("HBNB_TYPE_STORAGE") == "file"):
         @property
         def cities(self):
-            """Getter attribute of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            from models import storage
+            from models.city import City
+            mycities = {}
+            for city, value in storage.all(City).items():
+                if (value.state_id == self.id):
+                        mycities.update({city: value})
+            return mycities
