6dc186ad3d5d8291395df629d9b715f5a19319f1
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-11 20:59:03 +0000

    fixing sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,8 @@
 -- A database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- A new user hbnb_dev (in localhost)
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 -- hbnb_dev should have all privileges on the database hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- hbnb_dev should have SELECT privilege on the database performance_schema
