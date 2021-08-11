f4055e1e9cc96551ba74519fc7734e82a7166447
elyoguictm <2788@holbertonschool.com>
2021-08-11 13:34:46 -0700

    add

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Tests """
+""" Contains the class TestConsoleDocs """
+
 import pep8
 import unittest
-import cmd
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -13,12 +14,10 @@ from models.review import Review
 from models.engine.db_storage import DBStorage
 from os import getenv
 from console import HBNBCommand
-from models.engine.file_storage import FileStorage
 
 
-class TestConsole(unittest.TestCase):
-        """this will test the console"""
-
+class pep8_test(unittest.TestCase):
+    """pep8 test cases class"""
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
@@ -26,5 +25,17 @@ class TestConsole(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style Werrors (and warnings).")
 
-if __name__ == "__main__":
-        unittest.main()
+
+    def test_console_module_docstr(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(__import__("console").__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(__import__("console").__doc__) >= 1,
+                        "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstr(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
