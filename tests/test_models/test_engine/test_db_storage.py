2829dabcce511121fddae2c03fe28c5d1d465358
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-11 23:51:31 +0000

    adding db storage test

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""test for databasse storage"""
+import unittest
+from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from models import storage
+import MySQLdb
+import pep8
+
+
+class TestDBStorage(unittest.TestCase):
+    '''this will test the DBStorage'''
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "can't run if storage is file")
+    def setUp(self):
+        """set up for test"""
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            self.db = MySQLdb.connect(getenv("HBNB_MYSQL_HOST"),
+                                      getenv("HBNB_MYSQL_USER"),
+                                      getenv("HBNB_MYSQL_PWD"),
+                                      getenv("HBNB_MYSQL_DB"))
+            self.cursor = self.db.cursor()
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "can't run if storage is file")
+    def tearDown(self):
+        """at the end of the test this will close connection"""
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            self.db.close()
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
+                     "can't run if storage is file")
+    def test_pep8_DBStorage(self):
+        """Test for pep8"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+
+if __name__ == "__main__":
+    unittest.main()
