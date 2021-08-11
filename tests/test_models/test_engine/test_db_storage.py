9ad6638ffaef04c0357ffa9a49f914ded2d340a3
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-11 23:58:47 +0000

    adding test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -46,13 +46,30 @@ class TestDBStorage(unittest.TestCase):
 
     @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != 'db',
                      "can't run if storage is file")
-    def test_new_DBStorage(self):
-        """Tests for new() method"""
-        nb = self.cursor.execute("SELECT COUNT(*) FROM states")
-        s = State(name="Oregon")
-        s.save()
-        nb1 = self.cursor.execute("SELECT COUNT(*) FROM states")
-        self.assertEqual(nb1 - nb, 0)
+    def test_all_DBStorage(self):
+        """Tests for all() method"""
+        s = State(name="California")
+        storage.new(s)
+        storage.save()
+        k = '{}.{}'.format(type(s).__name__, s.id)
+        dic = storage.all(State)
+        self.assertTrue(k in dic.keys())
+        s1 = State(name="Arizona")
+        storage.new(s1)
+        storage.save()
+        k1 = '{}.{}'.format(type(s1).__name__, s1.id)
+        dic1 = storage.all()
+        self.assertTrue(k in dic1.keys())
+        self.assertTrue(k1 in dic1.keys())
+        u = User(email="derps@herps.com", password="hurrdurr")
+        storage.new(u)
+        storage.save()
+        k2 = '{}.{}'.format(type(u).__name__, u.id)
+        dic2 = storage.all(User)
+        self.assertTrue(k2 in dic2.keys())
+        self.assertFalse(k1 in dic2.keys())
+        self.assertFalse(k in dic2.keys())
+        self.assertFalse(k2 in dic.keys())
 
 if __name__ == "__main__":
     unittest.main()
