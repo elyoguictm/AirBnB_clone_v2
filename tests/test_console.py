25a777ba218682eeebaf13e4b319f7d850e09a0a
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-12 03:24:20 +0000

    fixing pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,13 +18,13 @@ from console import HBNBCommand
 
 class pep8_test(unittest.TestCase):
     """pep8 test cases class"""
+
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style Werrors (and warnings).")
-
 
     def test_console_module_docstr(self):
         """Test for the console.py module docstring"""
@@ -39,3 +39,17 @@ class pep8_test(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_create_no_class(self):
+        """create test w/o class"""
+        self.consola = HBNBCommand()
+        with patch('sys.stdout', new=StringIO()) as out:
+            self.consola.onecmd("create")
+        self.assertEqual("** class name missing **\n", out.getvalue())
+
+    def test_create_not_a_class(self):
+        """create test w/ non existing class"""
+        self.consola = HBNBCommand()
+        with patch('sys.stdout', new=StringIO()) as out:
+            self.consola.onecmd("create Danitkm")
+        self.assertEqual("** class doesn't exist **\n", out.getvalue())
