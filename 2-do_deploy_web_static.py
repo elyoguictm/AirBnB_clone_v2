15f17bd6eb9acdd9aeea9119f0d2366767b33b96
Ignacio Chitnisky <elinmortal@gmail.com>
2021-08-25 19:15:33 +0000

    all fixed?

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.api import env
 import re
 
 
-env.hosts = ['35.190.176.186', '35.196.156.157']
+env.hosts = ['34.74.93.165', '35.196.51.18']
 
 
 def do_pack():
@@ -32,29 +32,29 @@ def do_deploy(archive_path):
     res = put(archive_path, "/tmp/{}.tgz".format(filename))
     if res.failed:
         return False
-    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    res = run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
     if res.failed:
         return False
-    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+    res = run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
               .format(filename, filename))
     if res.failed:
         return False
-    res = run("rm /tmp/{}.tgz".format(filename))
+    res = run("sudo rm /tmp/{}.tgz".format(filename))
     if res.failed:
         return False
-    res = run("mv /data/web_static/releases/{}"
+    res = run("sudo mv /data/web_static/releases/{}"
               "/web_static/* /data/web_static/releases/{}/"
               .format(filename, filename))
     if res.failed:
         return False
-    res = run("rm -rf /data/web_static/releases/{}/web_static"
+    res = run("sudo rm -rf /data/web_static/releases/{}/web_static"
               .format(filename))
     if res.failed:
         return False
-    res = run("rm -rf /data/web_static/current")
+    res = run("sudo rm -rf /data/web_static/current")
     if res.failed:
         return False
-    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+    res = run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
               .format(filename))
     if res.failed:
         return False
